e058a9e068eedbe1fecc0dc7fa357012d2df30e8
QSuhack <graczgge@wp.pl>
2019-03-20 19:14:29 +0100

    d1

---
+++ informatyka_projekt/include.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <conio.h>
+#include <ctime>
+#include <vector>
+#include <stdlib.h>
+#include <algorithm>
+#include <fstream>
+#include <string>
+
+
+bool check_pesel(string pesel){
+};
--- informatyka_projekt/main.cpp
+++ informatyka_projekt/main.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <conio.h>
-#include <ctime>
-#include <vector>
-#include <stdlib.h>
-#include <algorithm>
-#include <fstream>
-#include <string>
+#include "include.h"
 
 using namespace std;
 struct osoba
@@ -27,7 +20,28 @@ void pobierz_dane(vector <osoba> &baza)
 	}
 
 }
+void dodaj_osobe(vector <osoba> &baza){
+	osoba os;
+	string os_tmp;
+	cout << "\nPodaj imiê: ";
+	cin >> os.imie;
+	cout << "\nPodaj nazwisko: ";
+	cin >> os.nazwisko; // TODO sprawdziæ czy nie puste etc.
+	bool flag = false;
+	while (!flag)
+	{
+		cout << "\nPodaj PESEL: ";
+		cin >> os_tmp;
+		flag = check_pesel(os_tmp);
+		if (!flag){
+			cout << "Niepoprawny format, spróbuj jeszcze raz!";
+		}
+	}
+	cout << "Podaj miasto: ";
+	cin >> os.miasto;
+	baza.push_back(os);
 
+}
 int main()
 {
 
